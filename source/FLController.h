aa399d0b7c097d5a0860a4bf7e863985d4d10b62
Dan Lonngren <danlonngren@gmail.com>
2025-07-13 18:17:23 +0100

    Copy working code from dlQuadcopter project

--- source/FLController.h
+++ source/FLController.h
@@ -1,3 +1,47 @@
 #pragma once
 
 #include "FLControllerInterface.h"
+
+#ifndef FUZZYCONTROLLER_H
+#define FUZZYCONTROLLER_H
+
+#include <functional>
+#include <vector>
+
+class FLController : public FLControllerInterface {
+	private:
+		float m_errorLimit;
+		float m_dErrorLimit;
+		float m_integralLimit;
+		float output_gain;
+		float m_outputMax;
+		float m_integralError;
+		float m_eLast;
+		float m_fuzzyOutput;
+		float m_w[4];
+		
+		struct Rule {
+			std::function<float()> condition; // Activation function
+			float weight;
+			float output;
+		};
+
+		// TODO: Change to array
+		std::vector<Rule> m_rules;
+	public:
+		FLController(float e_limit, float de_limit, float re_limit, float output_gain, float out_max);
+
+		void reset();
+		float evaluate(float input, float setpoint, bool Level);
+		void setLimits(float e_limit, float de_limit, float re_limit, float output_gain, float out_max);
+		void setOutputMax(float out_max);
+
+	private:
+		float normalize(float x, float limit) const;
+
+		// TODO: Fix
+		float positiveNL(float x);
+		float negativeNL(float x);
+};
+
+#endif
--- tests/FLControllerTests.cpp
+++ tests/FLControllerTests.cpp
@@ -11,6 +11,11 @@ protected:
     }
 };
 
-TEST_F(FLControllerTests, FLControllerZeroTest) {
-
+TEST_F(FLControllerTests, FLControllerResetTest) {
+    FLController controller(1.0f, 1.0f, 1.0f, 1.0f, 100.0f);
+    controller.reset();
+    
+    float output = controller.evaluate(0.0f, 0.0f, false);
+    
+    EXPECT_FLOAT_EQ(output, 0.0f);
 }
