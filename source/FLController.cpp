25229acffebefef59336d142817718f08a135fa1
Dan Lonngren <danlonngren@gmail.com>
2025-07-13 18:08:57 +0100

    Initial commit with empty structure

---
+++ source/FLController.cpp
@@ -0,0 +1,2 @@
+#include "FLController.h"
+
---
+++ tests/FLControllerTests.cpp
@@ -0,0 +1,16 @@
+
+#include "FLController.h"
+
+#include <gtest/gtest.h>
+
+class FLControllerTests : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+    void TearDown() override {
+    }
+};
+
+TEST_F(FLControllerTests, FLControllerZeroTest) {
+
+}
